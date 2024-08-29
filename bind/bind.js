f2498dacc307919b97f61c861783910e890086ef
Russ <wangqingqiang@xhzy.onaliyun.com>
2024-08-29 14:58:58 +0800

    feated: bind

---
+++ bind/bind.js
@@ -0,0 +1,35 @@
+//使用
+const obj = {
+  x: 42,
+  getX: function (name) {
+    console.log(name);
+    return this.x;
+  },
+};
+
+const unboundGetX = obj.getX;
+console.log(unboundGetX()); // The function gets invoked at the global scope
+// Expected output: undefined
+
+const boundGetX = unboundGetX.bind(obj);
+console.log(boundGetX());
+// Expected output: 42
+
+//手写实现
+// 不传参数版
+Function.prototype.myBind = function (context) {
+    const self = this
+    return function() {
+        return self.apply(context)
+    }
+}
+// 最终版
+Function.prototype.myBind = function (context) {
+    const self = this
+    //类数组arguments使用数组方法
+    const args = Array.prototype.slice.call(arguments, 1)
+    return function() {
+        const bindArgs = Array.prototype.slice.call(arguments);
+        return self.apply(context, args.concat(bindArgs))
+    }
+}
