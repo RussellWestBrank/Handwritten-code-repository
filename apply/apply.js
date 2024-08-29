4683f8787e1baf06940203a3a23273187d54d31a
Russ <wangqingqiang@xhzy.onaliyun.com>
2024-08-29 10:07:04 +0800

    feated: apply

---
+++ apply/apply.js
@@ -0,0 +1,25 @@
+// 使用
+const foo = {
+    value: 'foo',
+}
+function bar(name, age) {
+    console.log(name);
+    console.log(age);
+    console.log(this.value);
+}
+
+bar.apply(foo, ['wqq'])
+
+//手写实现
+Function.prototype.myApply= (context,agrs) => {
+    if(context === null || context === undefined){
+        context = window
+    }
+
+    const key = Symbol()
+    context[key] = this
+    const result = context.fn(...args)
+    delete context.fn
+
+    return result
+}
