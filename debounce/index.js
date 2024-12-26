24b59ab2d78c3a9c99c970781d40c201a34660b6
Russ <wangqingqiang@xhzy.onaliyun.com>
2024-12-26 21:16:48 +0800

    feat：加强防抖

--- debounce/index.js
+++ debounce/index.js
@@ -1,7 +1,7 @@
 //手写防抖函数
 function debounce(fn, delay) {
     let timer = null;
-    return function () {
+    return function (...arguments) {
         if (timer) {
             clearTimeout(timer);
         }
@@ -30,3 +30,49 @@ const debouncedLogName1 = debounce(obj.logName.bind(obj), 500);
 
 // 这时，即使直接调用 debouncedLogName，this 仍然指向 obj
 debouncedLogName1();  // 在延时500毫秒后会输出 "object"
+
+/**
+ * @param {Function} fn 需要防抖的函数
+ * @param {Object} option
+ * @param {Number} option.wait 等待时间
+ * @param {Boolean} option.leading 是否立即执行
+ * @param {Boolean} option.trailing 是否在等待时间结束后再执行
+ * @param {Number} option.maxWait 最大等待时间
+ * @returns {Function}
+ * */
+function debounce(fn, option) {
+    const { wait = 0, leading = false, trailing = true, maxWait } = option
+
+    let timer = null
+    let lastCallTime = 0
+
+    function debounced (...arguments) {
+        const time = Date.now()
+
+        const remainingTime = maxWait ? Math.min(maxWait- (time - lastCallTime)) : wait
+
+        if(leading && !timer) {
+            fn.apply(this, arguments)
+        }
+
+        if(timer) {
+            clearTimeout(timer)
+        }
+
+        if(trailing) {
+            timer = setTimeout(() => {
+                lastCallTime = Date.now()
+                fn.apply(this, arguments)
+            }, remainingTime)
+        }
+    }
+
+    debounced.cancel = () => {
+        if(timer) {
+            clearTimeout(timer)
+            timer = null
+        }
+    }
+
+    return debounced
+}
